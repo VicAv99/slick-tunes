b0da7d9f2738d29fe86cd68c02afd1d38a2a08f7
Victor Avila <vicav99@gmail.com>
2022-05-03 22:42:26 -0700

    chore: initial steps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,6 @@ import { Seo } from '@/components/Seo';
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
 
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
   return (
     <Layout>
